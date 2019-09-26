2693938538abe0ea5cb45e95eac6a87e6988768b
cvarela <cvarela@dattack.com>
2019-09-26 02:04:42 +0200

    Added support for Blob, Clob & XML types

--- src/main/java/com/dattack/dbcopy/engine/DataProvider.java
+++ src/main/java/com/dattack/dbcopy/engine/DataProvider.java
@@ -1,10 +1,10 @@
 package com.dattack.dbcopy.engine;
 
-import java.sql.Clob;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,41 +21,37 @@ public class DataProvider {
     private ResultSet resultSet;
     private DbCopyTaskResult taskResult;
     private volatile ArrayList<Integer> columns;
-    private boolean useClob;
-    private int columnSize;
     private ResultSetMetaData metadataCache;
 
-    public DataProvider(ResultSet resultSet, DbCopyTaskResult taskResult) throws SQLException {
+    DataProvider(ResultSet resultSet, DbCopyTaskResult taskResult) throws SQLException {
         this.resultSet = resultSet;
         this.metadataCache = resultSet.getMetaData();
         this.taskResult = taskResult;
-        this.useClob = false;
-        this.columnSize = 0;
     }
 
-    public ResultSetMetaData getMetaData() throws SQLException {
+    private ResultSetMetaData getMetaData() throws SQLException {
         return metadataCache;
     }
 
-    private synchronized ArrayList<Integer> getColumns(NamedParameterPreparedStatement preparedStatement)
+    private ArrayList<Integer> getColumns(NamedParameterPreparedStatement preparedStatement)
             throws SQLException {
 
         if (columns == null) {
 
-            final ArrayList<Integer> list = new ArrayList<>(getMetaData().getColumnCount());
+            synchronized (this) {
+                if (columns == null) {
+                    final ArrayList<Integer> list = new ArrayList<>(getMetaData().getColumnCount());
 
-            for (int columnIndex = 1; columnIndex <= getMetaData().getColumnCount(); columnIndex++) {
-                if (preparedStatement.hasNamedParameter(getMetaData().getColumnName(columnIndex))) {
-                    list.add(columnIndex);
-                    columnSize++;
-                    if (getMetaData().getColumnType(columnIndex) == Types.CLOB) {
-                        this.useClob = true;
+                    for (int columnIndex = 1; columnIndex <= getMetaData().getColumnCount(); columnIndex++) {
+                        if (preparedStatement.hasNamedParameter(getMetaData().getColumnName(columnIndex))) {
+                            list.add(columnIndex);
+                        } else {
+                            LOGGER.warn("Column {} not used", getMetaData().getColumnName(columnIndex));
+                        }
                     }
-                } else {
-                    LOGGER.warn("Column {} not used", getMetaData().getColumnName(columnIndex));
+                    columns = list;
                 }
             }
-            columns = list;
         }
 
         return columns;
@@ -63,17 +59,29 @@ public class DataProvider {
 
     private List<Object> retrieveData(NamedParameterPreparedStatement preparedStatement) throws SQLException {
 
-        List<Object> dataList = new ArrayList<>(columnSize);
+        ArrayList<Integer> _columns = getColumns(preparedStatement);
 
-        for (final int columnIndex : getColumns(preparedStatement)) {
+        List<Object> dataList = new ArrayList<>();
+
+        for (final int columnIndex : _columns) {
             final Object value = resultSet.getObject(columnIndex);
             if (resultSet.wasNull()) {
                 dataList.add(null);
             } else {
-                if (useClob && getMetaData().getColumnType(columnIndex) == Types.CLOB) {
-                    dataList.add(resultSet.getClob(columnIndex));
-                } else {
-                    dataList.add(value);
+
+                switch (getMetaData().getColumnType(columnIndex)) {
+                    case Types.CLOB:
+                        Clob sourceClob = resultSet.getClob(columnIndex);
+                        dataList.add(sourceClob.getSubString(1L, (int) sourceClob.length()));
+                        break;
+                    case Types.BLOB:
+                        dataList.add(resultSet.getBlob(columnIndex));
+                        break;
+                    case Types.SQLXML:
+                        dataList.add(resultSet.getSQLXML(columnIndex));
+                        break;
+                    default:
+                        dataList.add(value);
                 }
             }
         }
@@ -89,19 +97,69 @@ public class DataProvider {
         Iterator<Object> dataIterator = dataList.iterator();
         for (final int columnIndex : getColumns(preparedStatement)) {
             final Object value = dataIterator.next();
+            String columnName = getMetaData().getColumnName(columnIndex);
+            int columnType = getMetaData().getColumnType(columnIndex);
             if (value == null) {
-                preparedStatement.setNull(getMetaData().getColumnName(columnIndex),
-                        getMetaData().getColumnType(columnIndex));
+                preparedStatement.setNull(columnName, columnType);
             } else {
-                if (useClob && getMetaData().getColumnType(columnIndex) == Types.CLOB) {
-                    final Clob sourceClob = (Clob) value;
-                    preparedStatement.setObject(getMetaData().getColumnName(columnIndex),
-                            sourceClob.getSubString(0L, (int) sourceClob.length()));
-                } else {
-                    preparedStatement.setObject(getMetaData().getColumnName(columnIndex), value);
+                switch (columnType) {
+                    case Types.CLOB:
+                        populateClob(preparedStatement, (Clob) value, columnName);
+                        break;
+                    case Types.BLOB:
+                        populateBlob(preparedStatement, (Blob) value, columnName);
+                        break;
+                    case Types.SQLXML:
+                        populateSqlXml(preparedStatement, (SQLXML) value, columnName);
+                        break;
+                    default:
+                        preparedStatement.setObject(columnName, value);
                 }
             }
         }
+    }
+
+    private void populateBlob(NamedParameterPreparedStatement preparedStatement, Blob value, String columnName)
+            throws SQLException {
+
+        try {
+            Blob targetBlob = preparedStatement.getConnection().createBlob();
+            OutputStream output = targetBlob.setBinaryStream(1);
+
+            InputStream in = value.getBinaryStream();
+            byte[] buf = new byte[1024];
+            int len = 0;
+            while ((len = in.read(buf)) != -1) {
+                output.write(buf, 0, len);
+            }
+
+            preparedStatement.setBlob(columnName, targetBlob);
+            value.free();
+        } catch (IOException e) {
+            throw new SQLException("Unable to create Clob object: " + e.getMessage());
+        }
+    }
+
+    private void populateClob(NamedParameterPreparedStatement preparedStatement, Clob value, String columnName)
+            throws SQLException {
+
+        try {
+            Clob targetClob = preparedStatement.getConnection().createClob();
+            Writer clobWriter = targetClob.setCharacterStream(1);
+            clobWriter.write(value.getSubString(0L, (int) value.length()));
+            preparedStatement.setClob(columnName, targetClob);
+            value.free();
+        } catch (IOException e) {
+            throw new SQLException("Unable to create Clob object: " + e.getMessage());
+        }
+    }
+
+    private void populateSqlXml(NamedParameterPreparedStatement preparedStatement, SQLXML value, String columnName)
+            throws SQLException {
+        SQLXML targetXml = preparedStatement.getConnection().createSQLXML();
+        targetXml.setString(value.getString());
+        preparedStatement.setSQLXML(columnName, targetXml);
+        value.free();
     }
 
     boolean populateStatement(NamedParameterPreparedStatement preparedStatement) throws SQLException {
